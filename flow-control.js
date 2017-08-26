eed82de691a3215f5a2a60286d61eab101a82b96
giri kalra <giiirrriii@gmail.com>
2017-08-26 21:51:26 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -1,5 +1,7 @@
 function basicTeenager(age) {
-
+  if (age < 19 && age > 13) {
+    return "You are a teenager!";
+  }
 }
 
 function teenager(age) {
@@ -11,9 +13,10 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-
+//return age<20 && age >12 ? "You are a teenager!" : "You are not a teenager";
 }
-
 function switchAge(age) {
+
+  }
 
 }
