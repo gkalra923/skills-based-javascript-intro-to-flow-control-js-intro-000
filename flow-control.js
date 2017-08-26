a1aff12831e614c5ab62d4b7a40fcf7e405b2642
giri kalra <giiirrriii@gmail.com>
2017-08-26 22:04:08 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -1,32 +1,29 @@
-function basicTeenager(age){
-      if (age <= 19 && age => 13){
-        return "You are a teenager!";
-   }
- }
+function basicTeenager(age) {
+  if(age < 20 && age > 12) {
+    return "You are a teenager!";
+  }
+  else {
+    return "You are not a teenager";
+  }
+  
+}
 
 function teenager(age) {
-  if (age <= 19 && age => 13){
-     console.log("You are a teenager!");
-   }
-   else{
-     return "You are not a teenager";
-   }
+
+
 }
 
 function ageChecker(age) {
-  if (age <= 19 && age => 13){
-    console.log("You are a teenager!");
-  }
-  else if(age < 13){
-    console.log("You are a kid");
-  }
-  else{
-    return "You are a grownup";
-  }
+
+
+  
 }
 
 function ternaryTeenager(age) {
-age >= 13 && age <= 19 ? return "You are a teenager": "You are not a teenager"}
+
+
+}
+
 function switchAge(age) {
 
 
