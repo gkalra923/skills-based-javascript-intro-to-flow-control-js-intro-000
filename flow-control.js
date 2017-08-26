5991560500ad274803125d03b277176d7f8458ec
giri kalra <giiirrriii@gmail.com>
2017-08-26 21:54:33 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -1,8 +1,8 @@
-function basicTeenager(age) {
-  if (age < 19 && age > 13) {
-    return "You are a teenager!";
-  }
-}
+function basicTeenager(age){
+      if (age <= 19 && age => 13){
+        return "You are a teenager!";
+   }
+ }		  
 
 function teenager(age) {
 
